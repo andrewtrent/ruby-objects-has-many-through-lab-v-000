48f05ea6b189219cd20da433a4111d81116a5bf5
Andrew Trent <andrewtrentdesign@gmail.com>
2018-11-08 01:42:20 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,12 +1,16 @@
 class Doctor
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
 
   def new_appointment(patient, date)
     Appointment.new(patient, self, date)
+
   end
 
 end
